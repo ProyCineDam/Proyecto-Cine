9fdfcd8f761f9b39cd4b371446a846a339794e57
mati <mati@usuario-HP>
2016-06-02 09:33:15 +0200

    02/06/2016 9:33

--- src/gestion/Sala.java
+++ src/gestion/Sala.java
@@ -10,5 +10,41 @@ package gestion;
  * @author crist
  */
 public class Sala {
+    int numSala;
+    int filas;
+    int columnas;
+    boolean tresD;
+
+    public Sala(int numSala, int filas, int columnas) {
+        this.numSala = numSala;
+        this.filas = filas;
+        this.columnas = columnas;
+        tresD=false;
+    }
+
+    public int getNumSala() {
+        return numSala;
+    }
+
+    public void setNumSala(int numSala) {
+        this.numSala = numSala;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+    
     
 }
