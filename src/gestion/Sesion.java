03a7a32aaa6d92e4db4fbdba00d8ee8f526528ef
lliurex <lliurex@1asixdam>
2016-06-06 11:45:42 +0200

    06/06/2016 11:45

--- src/gestion/Sesion.java
+++ src/gestion/Sesion.java
@@ -8,7 +8,60 @@ public class Sesion {
     
     String nomSesion;
     Calendar fecha;
-    Calendar hora;
+    int hora;
     int sala;
+    double precio;
+    ArrayList<Asiento>asientos;
+
+    public Sesion(String nomSesion, Calendar fecha, int hora, int sala, double precio) {
+        this.nomSesion = nomSesion;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.sala = sala;
+        this.precio = precio;
+        asientos=new ArrayList<Asiento>();
+    }
+    public void añadirAsiento(Asiento asiento){
+        asientos.add(asiento);
+    }
+    public String getNomSesion() {
+        return nomSesion;
+    }
+
+    public void setNomSesion(String nomSesion) {
+        this.nomSesion = nomSesion;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Calendar fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getSala() {
+        return sala;
+    }
+
+    public void setSala(int sala) {
+        this.sala = sala;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
     
 }
